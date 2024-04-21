f69fd9b35753511ab3efbaf371f38a9b967b759a
Paleti Avinash <82360124+paletiavinash@users.noreply.github.com>
2024-04-21 22:12:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,33 +8,28 @@ terraform {
     }
   }
 }
+
 provider "aws" {
-  region = "ap-south-1"  
-  access_key = "AKIAQ3EGWLWKCEOXRHRJ"
-  secret_key = "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"
+  region = "ap-south-1"
 }
 
-
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
 }
-
 
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
 }
 
-# Create a subnet inside the VPC
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"  # Update with your desired subnet CIDR block
-  availability_zone = "ap-south-1a"  # Update with your desired AZ
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
-# Create a route table and associate it with the VPC
 resource "aws_route_table" "my_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
@@ -44,24 +39,39 @@ resource "aws_route_table" "my_route_table" {
   }
 }
 
-# Associate the route table with the subnet
 resource "aws_route_table_association" "subnet_association" {
   subnet_id      = aws_subnet.my_subnet.id
   route_table_id = aws_route_table.my_route_table.id
 }
 
-# Create an EC2 instance
-resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-001843b876406202a"  # Update with your desired AMI ID (Amazon Linux 2 as an example)
-  instance_type = "t2.micro"  # Update with your desired instance type
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.my_vpc.id
 
-  subnet_id = aws_subnet.my_subnet.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-001843b876406202a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+  security_groups = [aws_security_group.nginx_sg.name]
 
   tags = {
     Name = "MyEC2Instance"
   }
 
-  # Provision the EC2 instance using user data
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
